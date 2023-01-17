99707e234ffecb1dc9d21942cd09b6893b686184
Alexey-pyh <alexey.pyh132@gmail.com>
2023-01-17 17:56:25 +0300

    theed commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.work-formate-slider').slick({
+        slidesToShow: 1,
+        prevArrow: "<img src='/left.svg' class='prev' alt='1'>",
+        nextArrow: "<img src='/right.svg' class='next' alt='2'>",
+        height: 100
+    });
+});
