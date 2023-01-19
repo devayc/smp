a54a50a46d8ab2d767bc00898771dabbadf618ce
Alexey-pyh <alexey.pyh132@gmail.com>
2023-01-19 17:46:37 +0300

    bad-comite

--- js/index.js
+++ js/index.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
     $('.work-formate-slider').slick({
         slidesToShow: 1,
-        prevArrow: "<img src='/left.svg' class='prev' alt='1'>",
-        nextArrow: "<img src='/right.svg' class='next' alt='2'>",
+        prevArrow: "<img src='/image/left.svg' class='prev' alt='1'>",
+        nextArrow: "<img src='/image/right.svg' class='next' alt='2'>",
         height: 100
+    });
+
+    $('#slider').slick({
+        slidesToShow: 1,
+
+        height: 100,
+
+    });
+    $('.invite-formate-slider').slick({
+        slidesToShow: 1,
+        asNavFor: "#slider",
+        height: 100,
+        prevArrow: "<img src='/image/left.svg' class='prev' alt='1'>",
+        nextArrow: "<img src='/image/right.svg' class='next' alt='2'>",
+
     });
 });
